c7c4e51ba2e0663f548419e7880b6093fab45d88
NaneA <31791931+NaneA@users.noreply.github.com>
2021-07-24 00:31:30 +0400

    hw3

---
+++ week3/IntBuffer.java
@@ -0,0 +1,18 @@
+package com.company.week3;
+
+public class IntBuffer {
+
+    int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString(){
+        return  this.value+"";
+    }
+}
---
+++ week3/LinkedIntBuffer.java
@@ -0,0 +1,28 @@
+package com.company.week3;
+
+public class LinkedIntBuffer extends IntBuffer {
+    LinkedIntBuffer next;
+    private int counter;
+
+
+    public LinkedIntBuffer getNext(){
+        return next;
+    }
+
+    public void setNext(){
+        this.next = next;
+        this.counter++;
+    }
+   @Override
+   public String toString(){
+       LinkedIntBuffer cur = next;
+
+       for(int i = 0; i<counter; i++)
+       {
+           System.out.println(cur.getValue() + "");
+           cur = cur.getNext();
+       }
+
+        return "";
+   }
+}
