8f4c0507be0cd5a547ca69f2af76a05927e0df60
NaneA <nane.k.abrahamyan@gmail.com>
2021-08-25 11:24:02 +0400

    part1

--- week4-hw/src/task3/Printer.java
+++ week4-hw/src/task3/Printer.java
@@ -1,2 +1,14 @@
-package task3;public class Printer {
+package task3;
+import task1.*;
+
+public class Printer {
+
+    static void specialPrint(Object o){
+
+        System.out.println("**********************");
+        System.out.println(o.toString());
+        System.out.println("**********************");
+
+    }
+
 }
