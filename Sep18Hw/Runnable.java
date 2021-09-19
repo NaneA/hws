ae5a6ea47e5833a73717a37f51c261f3b2f406ee
NaneA <nane.k.abrahamyan@gmail.com>
2021-09-19 19:22:15 +0400

    update

--- Sep18Hw/Runnable.java
+++ Sep18Hw/Runnable.java
@@ -1,2 +1,21 @@
-package Sep18Hw;public class Runnable {
+package Sep18Hw;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Runnable {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\Nane\\Desktop\\adobe\\hws\\Sep18Hw\\file");
+        new Thread(new java.lang.Runnable() {
+            PrintWriter printWriter = new PrintWriter(file);
+            @Override
+            public void run() {
+                for (int i = 0; i <10000 ; i++) {
+                    printWriter.println(" " + i);
+                }
+                printWriter.close();
+            }
+        }).start();
+    }
 }
