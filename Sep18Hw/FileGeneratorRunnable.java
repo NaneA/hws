e971959b34aa1cf93b0e4caf4e7b9d1c0c0a8090
NaneA <nane.k.abrahamyan@gmail.com>
2021-09-19 17:41:20 +0400

    task1

---
+++ Sep18Hw/FileGeneratorRunnable.java
@@ -0,0 +1,47 @@
+package Sep18Hw;
+
+import java.io.*;
+import java.util.UUID;
+
+public class FileGeneratorRunnable {
+
+
+    public static void main(String[] args) {
+
+
+        new Thread(() -> {
+            FileWriter file = null;
+            System.out.println("in the thread");
+            for (int i = 0; i < 10; i++) {
+                System.out.println("in the first loop");
+                try {
+                    file = new FileWriter("C:\\Users\\Nane\\Desktop\\adobe\\hws\\Sep18Hw\\" + i);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+              //  Thread.yield();
+                print(file);
+                try {
+                    file.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }).start();
+
+    }
+
+    synchronized static void print(FileWriter file) {//synchronized method
+        for (int i = 1; i <= 10000; i++) {
+           // System.out.println(n * i);
+            try {
+                file.write(i+"\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
